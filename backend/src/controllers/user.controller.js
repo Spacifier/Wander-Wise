c9abddab1e186d51c52bf40c4c98727ece8b2116
Swapnil Garg <swapnilgarg2004@gmail.com>
2025-08-04 02:11:18 +0530

    Fixing cloudinary delete avatar

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 const options = {
     httpOnly: true,
     secure: true, // Important for HTTPS
